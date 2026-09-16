3753c9be99fd4099769e6987dfe2fd9583e0e0c5
thenamelessdev <aron.minarovits@gmail.com>
2026-09-16 18:16:04 +0200

    feat: create backend

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,16 @@
+import { fail } from '@sveltejs/kit';
+import type { Actions } from './$types';
+import { createShortenedURL } from '$lib/createShortenedURL';
+
+export const actions: Actions = {
+	create: async ({ request }) => {
+		const formData = await request.formData();
+
+		const url = formData.get('url') as string | undefined;
+		if (!url) return fail(400, 'No URL');
+
+		const id = await createShortenedURL(url);
+
+		return { id };
+	}
+};
