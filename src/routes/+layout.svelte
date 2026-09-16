696f1ab38ae82e966f464b28ba513e6b38a31de2
thenamelessdev <aron.minarovits@gmail.com>
2026-09-16 17:33:54 +0200

    init sveltekit project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import './layout.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><title>Nameless link</title></svelte:head>
+
+<main class="m-3">
+	{@render children()}
+</main>
