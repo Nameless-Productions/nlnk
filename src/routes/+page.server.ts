7e7869925ce904f398af2c259ba8fcbc438a90c0
thenamelessdev <aron.minarovits@gmail.com>
2026-09-16 19:33:37 +0200

    feat: invalid url checking

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,12 +2,23 @@ import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
 import { createShortenedURL } from '$lib/createShortenedURL';
 
+function isValidURL(url: string): boolean {
+	try {
+		new URL(url);
+		return true;
+	} catch {
+		return false;
+	}
+}
+
 export const actions: Actions = {
 	create: async ({ request }) => {
 		const formData = await request.formData();
 
 		const url = formData.get('url') as string | undefined;
 		if (!url) return fail(400, { error: 'No URL' });
+
+		if (!isValidURL(url)) return fail(400, { error: 'Invalid URL' });
 
 		const id = await createShortenedURL(url);
 
