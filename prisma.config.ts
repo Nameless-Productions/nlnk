dc8f026582d50a920b708eae5d4c194c36499070
thenamelessdev <aron.minarovits@gmail.com>
2026-09-25 09:43:57 +0200

    feat: switch to latest prisma version

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,12 @@
 import 'dotenv/config';
+import { definePrismaConfig } from 'prisma/config';
+import { defineConfig as ormConfig } from '@prisma/orm-postgres/config';
 
-export default {
-	schema: 'prisma/schema.prisma',
-	datasource: {
-		url: process.env['DATABASE_URL']
-	}
-};
+export default definePrismaConfig({
+	orm: ormConfig({
+		contract: './src/lib/contract.prisma',
+		db: {
+			connection: process.env['DATABASE_URL']!
+		}
+	})
+});
--- prisma/migrations/20260916154203_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateTable
-CREATE TABLE "Links" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "link" TEXT NOT NULL
-);
